c3af58d2497f5cef02bb05bcb6545924a38d0e3d
Rafael Pianaro <rafaelpianaro@gmail.com>
2024-04-14 16:54:11 -0300

    feat: verifyEmail working

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,9 @@
 
 namespace App\Providers;
 
+use Illuminate\Auth\Notifications\VerifyEmail;
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Support\Facades\Lang;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -19,6 +22,12 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        VerifyEmail::toMailUsing(function (object $notifiable, string $url) {
+            return (new MailMessage)
+                    ->subject("Verify Email Address")
+                    ->greeting(Lang::get('Hello') . ' ' . $notifiable->name . ',')
+                    ->line('Click the button below to verify your email address.')
+                    ->action('Verify Email Address', $url);
+        });
     }
 }
